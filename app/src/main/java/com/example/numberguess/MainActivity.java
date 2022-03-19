49015c931fcd69684e001026cf0165baef81a59e
Harsh <harshkuddu63@gmail.com>
2022-03-18 22:48:16 -0400

    button clicking

--- app/src/main/java/com/example/numberguess/MainActivity.java
+++ app/src/main/java/com/example/numberguess/MainActivity.java
@@ -17,41 +17,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonStart=(Button) findViewById(R.id.start);
+        buttonStart=findViewById(R.id.start);
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                openStartActivity();
+                Intent intent=new Intent(MainActivity.this,GameActivity.class);
+                startActivity(intent);
             }
 
 
         });
-        buttonInstructions=(Button) findViewById(R.id.instructions);
+        buttonInstructions=findViewById(R.id.instructions);
         buttonInstructions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                openInstructionsActivity();
+                Intent intent=new Intent(MainActivity.this,InstructionsActivity.class);
+                startActivity(intent);
             }
         });
 
-        buttonHigh=(Button) findViewById(R.id.highscores);
+        buttonHigh=findViewById(R.id.highscores);
         buttonHigh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                openHighActivity();
+                Intent intent=new Intent(MainActivity.this,ScoresActivity.class);
+                startActivity(intent);
             }
         });
     }
-    public void openStartActivity(){
-        Intent intent= new Intent(this,GameActivity.class);
-        startActivity(intent);
-    }
-    public void openInstructionsActivity(){
-        Intent intent= new Intent(this,InstructionsActivity.class);
-        startActivity(intent);
-    }
-    public void openHighActivity(){
-        Intent intent= new Intent(this,ScoresActivity.class);
-        startActivity(intent);
-    }
+
 }
