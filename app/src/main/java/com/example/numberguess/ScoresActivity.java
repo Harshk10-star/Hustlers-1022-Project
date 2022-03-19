60e7a3fdee95b20a12622e015f313811505e68d9
Harsh <harshkuddu63@gmail.com>
2022-03-19 10:53:06 -0400

    added button function

--- app/src/main/java/com/example/numberguess/ScoresActivity.java
+++ app/src/main/java/com/example/numberguess/ScoresActivity.java
@@ -2,13 +2,27 @@ package com.example.numberguess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScoresActivity extends AppCompatActivity {
-
+    private Button homePage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+        homePage=(Button) findViewById(R.id.homepage);
+        homePage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(ScoresActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
     }
 }
