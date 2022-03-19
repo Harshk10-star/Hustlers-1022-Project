8a771320f0bef5d059c295c27424ad2b0d476bbf
Harsh <harshkuddu63@gmail.com>
2022-03-19 11:53:55 -0400

    stuff

--- app/src/main/java/com/example/numberguess/LevelActivity.java
+++ app/src/main/java/com/example/numberguess/LevelActivity.java
@@ -2,13 +2,27 @@ package com.example.numberguess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LevelActivity extends AppCompatActivity {
+    private Button tohome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
+        tohome=(Button) findViewById(R.id.homeFromLevel);
+        tohome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(LevelActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
     }
 }
