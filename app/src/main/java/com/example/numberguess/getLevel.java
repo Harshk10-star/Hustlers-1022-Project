97280e69538fc6fde9cccca8ad0e2ebfafbe45a6
Harsh <harshkuddu63@gmail.com>
2022-04-01 18:46:45 -0400

    level

--- app/src/main/java/com/example/numberguess/getLevel.java
+++ app/src/main/java/com/example/numberguess/getLevel.java
@@ -1,18 +1,25 @@
 package com.example.numberguess;
-
+import java.util.Random;
 public class getLevel {
 
     public static double easy(){
-
-        return 10;
+        Random rand=new Random();
+        int easy=rand.nextInt(10)+1;
+        return easy;
     }
     public static double medium(){
-        return 5;
+        Random rand=new Random();
+        int medium=rand.nextInt(29)+1;
+        return medium;
     }
     public static double hard(){
-        return 5;
+        Random rand=new Random();
+        int hard=rand.nextInt(49)+1;
+        return hard;
     }
     public static double extreme(){
-        return 5;
+        Random rand=new Random();
+        int extreme=rand.nextInt(99)+1;
+        return extreme;
     }
 }
