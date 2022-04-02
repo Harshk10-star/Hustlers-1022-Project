20c697219658ff9cae6f78a89a6932b6fb7606d0
Harsh <harshkuddu63@gmail.com>
2022-04-01 20:25:01 -0400

    new changes

--- app/src/main/java/com/example/numberguess/GameActivity.java
+++ app/src/main/java/com/example/numberguess/GameActivity.java
@@ -12,70 +12,40 @@ import android.widget.TextView;
 public class GameActivity extends AppCompatActivity {
 
 
-    private EditText guess;
-    private CheckBox hintReveal;
-    private EditText hint;
-    private Button toHome;
-    private Button Guess;
-    private View listener;
-    private int randomNum;
     private String level;
     private int number;
-
-    public GameActivity(String level){
-        this.level=level;
-        if(level=="easy"){
-            number=getLevel.easy();
-        }else if(level=="medium"){
-            number=getLevel.medium();
-        }else if(level=="hard") {
-            number = getLevel.hard();
-        }else if(level=="extreme") {
-            number = getLevel.extreme();
-        }
-    }
-
+    private Button getNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-
-
-
-    }
-
-    public void  onClickOther(View v){
-        EditText textInput= (EditText) findViewById(R.id.guessText);
-        String convertTextToString=textInput.getText().toString();
-
-
-        int userChoice=getLevel.toInteger(convertTextToString);
-
-        if(userChoice==number){
-            if(level=="easy"){
-                number=getLevel.easy();
-            }else if(level=="medium"){
-                number=getLevel.medium();
-            }else if(level=="hard") {
-                number = getLevel.hard();
-            }else if(level=="extreme") {
-                number = getLevel.extreme();
+        getNumber=(Button) findViewById(R.id.guessButton);
+        getNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText textInput= (EditText) findViewById(R.id.guessText);
+                String convertTextToString=textInput.getText().toString();
+                int userChoice=getLevel.toInteger(convertTextToString);
+                if(userChoice==number){
+                    if(level=="easy"){
+                        number=getLevel.easy();
+                    }else if(level=="medium"){
+                        number=getLevel.medium();
+                    }else if(level=="hard") {
+                        number = getLevel.hard();
+                    }else if(level=="extreme") {
+                        number = getLevel.extreme();
+                    }
+                    String sendBack="Good Job! Guess next number!";
+                    ((TextView)findViewById(R.id.response)).setText(sendBack);
+                }else{
+                    String sendBack="Incorrect try agai!";
+                    ((TextView)findViewById(R.id.response)).setText(sendBack);
+                }
             }
-            String sendBack="Good Job! Guess next number!";
-            ((TextView)findViewById(R.id.response)).setText(sendBack);
-        }else{
-            String sendBack="Incorrect try agai!";
-            ((TextView)findViewById(R.id.response)).setText(sendBack);
-        }
-
+        });
 
     }
-
-
-
-
-
-
 }
