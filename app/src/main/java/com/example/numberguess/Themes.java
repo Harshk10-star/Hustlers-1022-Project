e80acaaec33f544f880086031ac2b9f11b8c04f9
N-acker <77125698+N-acker@users.noreply.github.com>
2022-04-02 15:31:03 -0400

    Nathaniel  background change

--- app/src/main/java/com/example/numberguess/Themes.java
+++ app/src/main/java/com/example/numberguess/Themes.java
@@ -1,73 +1,76 @@
 package com.example.numberguess;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Themes extends AppCompatActivity {
 
-    private View background;
-    private Button tech;
-    private Button storm;
-    private Button galaxy;
-    private Button  none;
+//    private Button tech;
+//    private Button storm;
+//    private Button galaxy;
+//    private Button  none;
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_themes);
+//
+//            final RelativeLayout background;
+//
+//            background=findViewById(R.id.themesBack);
+//
+//            tech=(Button) findViewById(R.id.techButton);
+//            tech.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//
+//
+//                    background.setBackgroundResource(R.drawable.tech);
+//
+//                }
+//
+//            });
 
 
-            background = (View)findViewById(R.id.themesBack);
-
-            tech=(Button) findViewById(R.id.techButton);
-            tech.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-
-                    background.setBackground();
-
-                }
-
-            });
-
-
-            storm=(Button) findViewById(R.id.stormButton);
-            storm.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-
-
-                }
-
-            });
-
-
-            galaxy=(Button) findViewById(R.id.galaxyButton);
-            galaxy.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-
-
-                }
-
-            });
-
-
-
-            none=(Button) findViewById(R.id.none);
-            none.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-
-
-                }
-
-            });
+//            storm=(Button) findViewById(R.id.stormButton);
+//            storm.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//
+//
+//                }
+//
+//            });
+//
+//
+//            galaxy=(Button) findViewById(R.id.galaxyButton);
+//            galaxy.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//
+//
+//                }
+//
+//            });
+//
+//
+//
+//            none=(Button) findViewById(R.id.none);
+//            none.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//
+//
+//                }
+//
+//            });
 
 
         }
